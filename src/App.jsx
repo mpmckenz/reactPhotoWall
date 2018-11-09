017e517074325c24079533de88bb6606493a1c5b
Michael McKenzie <mikemckenzie987@gmail.com>
2018-11-09 13:34:57 -0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,43 +8,42 @@ const PHOTO_URL = "https://picsum.photos/200?photo=";
 const PHOTO_LIST_URL = "https://picsum.photos/list";
 
 class App extends Component {
-  // 1. Declare a state object that will be used to track an array of photos
-  state = {};
-  // 2. Declare a life cycle method
-  // This life cycle method should:
-  //  - will be called after the component is initially rendered
-  // - will fetch an array of photos
-  // - will add that array of photos to state once received
+  state = { photos: [] };
+  // , numberOfPhotostoRender: 10
+
+  // handleLoadMoreImages() {
+  //   this.setState(numberOfPhotostoRender += 10)
+  // }
+
+  componentDidMount() {
+    fetch(PHOTO_LIST_URL)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(photoArr => this.setState({ photos: photoArr }));
+  }
   render() {
     const { photos = [] } = this.state;
     return (
       <React.Fragment>
         <header>
           <h1>Photo Wall</h1>
+          <hr />
           <p>
-            Start by reading App.jsx and completing the numbered steps.
-            Afterward, delete this paragraph. Then, open up App.css and complete
-            the instructions there.
+            Alas, I have conquered the foregrounds of React. Never shall I turn
+            back!
           </p>
         </header>
         <div className="collage">
-          componentDidMount() {
-          {fetch(PHOTO_LIST_URL)
-            .then(function(response) {
-              return response.json();
-            })
-          /* { We use map here because Array.prototype.map is an expression,
-            and for loops are not. You'll learn more about this soon!
-           } */
-           {photos.map(photo => (
-             <img
-               alt={/* 3. Fill me in with the photo's filename */ "/?random"}
-               key={/* 4. Fill me in with the photo's id */ "/200"}
-               src={/* 5. Fill me in with the photo's URL */ "https://picsum.photos"}
-             />
-           ))}
-          }
-        }
+          {/* slice(0, numberOfPhotostoRender)  */}
+          {photos.map(photo => (
+            <img
+              id={"allPhotos"}
+              alt={photo.filename}
+              key={photo.id}
+              src={PHOTO_URL + photo.id}
+            />
+          ))}
         </div>
       </React.Fragment>
     );
